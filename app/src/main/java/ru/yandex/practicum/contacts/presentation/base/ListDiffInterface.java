e2a49d2c8a9d599529603533536f1cc7590205d1
getastep <parshin@dl-audio.ru>
2023-03-12 13:54:24 +0300

    Sprint 2 final project V.1

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+public interface ListDiffInterface<T> {
+
+    boolean theSameAs(T newItem);
+
+    @Override
+    boolean equals(Object o);
+
+}
